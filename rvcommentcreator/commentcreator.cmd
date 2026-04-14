9ca0737cea358fab60af58c670a9271e91d8df0e
William <viliamoleosdev@gmail.com>
2026-04-15 00:53:57 +0500

    commentcreator.cmd*>(new styling)

--- rvcommentcreator/commentcreator.cmd
+++ rvcommentcreator/commentcreator.cmd
@@ -1,29 +1,69 @@
 @echo off
 
-rem Config:
-SET CHAR=/
+::::::::::::::::::::::::::::::::::::::::: CONFIG :::::::::::::::::::::::::::::::::::::::::
+
+SET "PREFIX=/*"
+SET "BAR=~"
+SET "POSTFIX=*/"
+
+SET "MIDPREFIX=[ "
+SET "MIDPOSTFIX= ]"
+
+SET "COUNT=90"
+
+:::::::::::::::::::::::::::::::::::::::::: MAIN ::::::::::::::::::::::::::::::::::::::::::
 
 ECHO.
 ECHO.  This script shall create a header-style comment for this codebase.
 ECHO Input a comment label which will be in the middle of the comment.
 ECHO Make sure Your label isn't ludicrously big and has an even number of characters.
-ECHO Example comment: %CHAR%%CHAR%%CHAR%...%CHAR%%CHAR%%CHAR% MAIN %CHAR%%CHAR%%CHAR%...%CHAR%%CHAR%%CHAR%
+ECHO Example comment: %PREFIX%%BAR%%BAR%%BAR%...%BAR%%BAR%%BAR%%MIDPREFIX% MAIN %MIDPOSTFIX%%BAR%%BAR%%BAR%...%BAR%%BAR%%BAR%%POSTFIX%
 ECHO.
 SET /p label="Comment Label: "
 ECHO.
 
-rem Count characters in label by putting it in a temp file and counting bytes in file:
-SET tempfile=temp.txt
-ECHO %label%>%tempfile%
-FOR /F "usebackq" %%i in ('%tempfile%') do SET /a chc=%%~zi - 2
-del %tempfile%
+::: === var count === :::
+rem Count characters in config variables by putting them in a temp file and counting bytes
 
-rem Calculate and print out the comment:
-SET /a wingc=43-%chc%/2
-FOR /L %%i in (1,1,%wingc%) do ECHO | SET /p=%CHAR%
-ECHO. | SET /p="%CHAR% %label% %CHAR%"
-FOR /L %%i in (1,1,%wingc%) do ECHO | SET /p=%CHAR%
+	SETLOCAL enabledelayedexpansion
+
+CALL:varlenfunc PREFIX
+	SET PREFIX_siz=%return%
+CALL:varlenfunc MIDPREFIX
+	SET MIDPREFIX_siz=%return%
+CALL:varlenfunc label
+	SET label_siz=%return%
+
+::: === print === :::
+SET /a wingc=(%COUNT%-%label_siz%)/2 - %PREFIX_siz% - %MIDPREFIX_siz%
+
+ECHO|SET /p="%PREFIX%"
+	FOR /L %%i in (1,1,%wingc%) do ECHO|SET /p="!BAR!"
+ECHO|SET /p="%MIDPREFIX%%label%%MIDPOSTFIX%"
+	FOR /L %%i in (1,1,%wingc%) do ECHO|SET /p="!BAR!"
+ECHO %POSTFIX%
 
 ECHO.
 ECHO.
 ECHO Generation done! Copy the comment above to use it!
+
+goto:eof
+
+:::::::::::::::::::::::::::::::::::::::::: FUNC ::::::::::::::::::::::::::::::::::::::::::
+
+rem %1 - inspected variable
+rem uses "vl_tempfile" variable, temp.tmp file, returns "return"
+:varlenfunc
+
+	SET vl_tempfile=temp.tmp
+	ECHO !%1!>%vl_tempfile%
+
+	FOR /f "usebackq" %%i in ('%vl_tempfile%') do SET /a return=%%~zi-2
+
+	DEL %vl_tempfile%
+
+goto:eof
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: passable stuff; bar flip flop
